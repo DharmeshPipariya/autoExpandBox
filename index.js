b2162dd4d28308614281d1a29d0f95b702d1fda2
Dharmesh <pipariyadharmesh@gmail.com>
2015-10-29 10:07:19 +0530

    update performance.
    update performance to prevent unnecessary move and mouse up event function call on document.

--- index.js
+++ index.js
@@ -4,37 +4,35 @@ app.directive('autoExpand', ['$interval', function ($interval) {
     return {
         restrict: 'A',
         link: function ($scope, elem, attr) {
-            $scope.down = false;
-            $scope.drag = false;
             $scope.timer;
-            elem.on('mousedown', function () { $scope.down = true; });
-            angular.element(document).on('mousemove', function (e) {
-                if ($scope.down) {
-                    var offset = elem.offset();
-                    var width = elem.width();
-                    var height = elem.height();
-                    if (width - 50 < (e.pageX - offset.left) || height - 50 < (e.pageY - offset.top)) {
-                        $interval.cancel($scope.timer);
-                        $scope.timer = $interval(function () {
-                            if (width - 50 < (e.pageX - offset.left)) {
-                                elem.css('width', elem.width() + 4);
-                                elem.parent().scrollLeft(width + 10);
-                            }
-                            if (height - 50 < (e.pageY - offset.top)) {
-                                elem.css('height', elem.height() + 4);
-                                elem.parent().scrollTop(height + 10);
-                            }
-                        }, 100);
-                    } else {
-                        $interval.cancel($scope.timer);
-                    }
+            elem.on('mousedown', function () {
+                angular.element(document).on('mousemove', moveCursor).on('mouseup', upCursor);
+            });
+
+            var moveCursor = function (e) {
+                var offset = elem.offset();
+                var width = elem.width();
+                var height = elem.height();
+                if (width - 50 < (e.pageX - offset.left) || height - 50 < (e.pageY - offset.top)) {
+                    $interval.cancel($scope.timer);
+                    $scope.timer = $interval(function () {
+                        if (width - 50 < (e.pageX - offset.left)) {
+                            elem.css('width', elem.width() + 4);
+                            elem.parent().scrollLeft(width + 10);
+                        }
+                        if (height - 50 < (e.pageY - offset.top)) {
+                            elem.css('height', elem.height() + 4);
+                            elem.parent().scrollTop(height + 10);
+                        }
+                    }, 100);
                 } else {
                     $interval.cancel($scope.timer);
                 }
-            }).on('mouseup', function () {
-                $scope.down = false;
+            };
+            var upCursor = function (e) {
+                angular.element(document).off('mousemove', moveCursor).off('mouseup', upCursor);
                 $interval.cancel($scope.timer);
-            });
+            };
         }
     };
 }]);
